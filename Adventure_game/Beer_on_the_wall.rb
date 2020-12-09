76c919d0a376dcdb0751e719730a213ea7b31d79
donut211 <andrewlasren10@gmail.com>
2020-12-08 22:36:00 -0600

    Adds an easter egg into the Combat module

---
+++ Adventure_game/Beer_on_the_wall.rb
@@ -0,0 +1,25 @@
+def sing_lyrics_A (current_beer)
+    puts "#{current_beer} bottles of beer on the wall,"
+    puts "#{current_beer} bottles of beer,"
+    puts "You take 1 down,\nand pass it around,"
+  end
+  
+  def sing_lyrics_B(current_beer)
+    puts "#{current_beer} bottles of beer on the wall!"
+    puts "*" * 80
+  end
+  
+  def one_left(current_beer)
+    puts "#{current_beer} more bottle of beer on the wall,"
+    puts "#{current_beer} more bottle of beer,"
+    puts "You take it down,\nand pass it around,"
+    puts "You ran out of bottles of beer on the wall!"
+  end
+
+current_beer = 99
+while current_beer > 1
+  sing_lyrics_A(current_beer)
+  current_beer -= 1
+  sing_lyrics_B(current_beer)
+end
+one_left(1)
